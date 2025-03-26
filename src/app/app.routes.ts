a386c91419899270f28f88a9ed021cd8fc3cf3fa
kripalsinh padhariya <kripalsinhp19@gmail.com>
2025-03-26 11:55:34 +0530

    *done*

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent },
+
+  { path: 'header', component: HeaderComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
